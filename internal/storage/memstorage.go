ca22187435522c642c2d688bf3e99aec73a08b71
Dmitry Krivenko <dmitrijs2005@gmail.com>
2025-01-28 23:19:21 -0500

    Add mutex

--- internal/storage/memstorage.go
+++ internal/storage/memstorage.go
@@ -3,12 +3,14 @@ package storage
 import (
 	"errors"
 	"fmt"
+	"sync"
 
 	"github.com/dmitrijs2005/metric-alerting-service/internal/metrics"
 )
 
 type MemStorage struct {
 	Data map[string]metrics.Metric
+	mu   sync.Mutex
 }
 
 func getKey(metricType metrics.MetricType, metricName string) string {
@@ -21,6 +23,10 @@ func NewMemStorage() *MemStorage {
 
 func (s *MemStorage) Retrieve(metricType metrics.MetricType, metricName string) (metrics.Metric, error) {
 	key := getKey(metricType, metricName)
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	if value, exists := s.Data[key]; exists {
 		return value, nil
 	} else {
@@ -29,7 +35,12 @@ func (s *MemStorage) Retrieve(metricType metrics.MetricType, metricName string) 
 }
 
 func (s *MemStorage) RetrieveAll() ([]metrics.Metric, error) {
+
 	result := []metrics.Metric{}
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	for _, metric := range s.Data {
 		result = append(result, metric)
 	}
@@ -38,6 +49,10 @@ func (s *MemStorage) RetrieveAll() ([]metrics.Metric, error) {
 
 func (s *MemStorage) Add(metric metrics.Metric) error {
 	key := getKey(metric.GetType(), metric.GetName())
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	_, exists := s.Data[key]
 	if exists {
 		return errors.New(MetricAlreadyExists)
@@ -47,5 +62,9 @@ func (s *MemStorage) Add(metric metrics.Metric) error {
 }
 
 func (s *MemStorage) Update(metric metrics.Metric, value interface{}) error {
+
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
 	return metric.Update(value)
 }
