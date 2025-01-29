5ee821112b9888b9ec5c7b91b0355c59eae11d72
Dmitry Krivenko <dmitrijs2005@gmail.com>
2025-01-29 00:38:54 -0500

    Add simple agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import "github.com/dmitrijs2005/metric-alerting-service/internal/agent"
+
+func main() {
+
+	pollInterval := 2
+	reportInterval := 10
+	serverUrl := "http://localhost:8080"
+
+	a := agent.NewMetricAgent(pollInterval, reportInterval, serverUrl)
+	a.Run()
+}
